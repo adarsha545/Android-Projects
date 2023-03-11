3034efb768e3b5c33a6ce3a3247d0367f2d51976
adarsha545 <adarshaghosh545@gmail.com>
2023-03-11 19:51:28 +0530

    commit

---
+++ burger_king/lib/widgets/items_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
